d4a907132a791ab00e2f401c68119ca48a10fce9
Gerald Franco <ojeff9941@gmail.com>
2024-03-01 13:43:51 +0000

    push

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -6,17 +6,24 @@ function runProgram(){
   ////////////////////////////////////////////////////////////////////////////////
   //////////////////////////// SETUP /////////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
-
+  var walker = {
+    "positionX": 0,
+    "positionY": 0,
+    "speedX": 0,
+    "speedY": 0
+  };
   // Constant Variables
   var FRAME_RATE = 60;
   var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
   
   // Game Item Objects
-
+  var borderWidth = 400;
+  var borderHeight = 400;
 
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
+  $(document).on('keydown', handleKeyDown);
+  $(document).on('keyup', handleKeyUp);                          // change 'eventType' to the type of event you want to handle
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -27,22 +34,89 @@ function runProgram(){
   by calling this function and executing the code inside.
   */
   function newFrame() {
-    
-
+    repositionGameItem();
+    redrawGameItem();
+    wallCollision();
   }
-  
+  var KEY = {
+    UP: 38,
+    DOWN: 40,
+    LEFT: 37,
+    RIGHT: 39,
+  };
   /* 
   Called in response to events.
   */
-  function handleEvent(event) {
-
+  function handleKeyDown(event) {
+    if (event.which === KEY.UP) {
+      walker.speedY = -5;
+      console.log("Up Arrow Pressed");
+    }  
+    if (event.which === KEY.DOWN) {
+      walker.speedY = 5;
+      console.log("Down Arrow Pressed");
+    }  
+    if (event.which === KEY.LEFT) {
+      walker.speedX = -5;
+      console.log("Left Arrow Pressed");
+    }  
+    if (event.which === KEY.RIGHT) {
+      walker.speedX = 5;
+      console.log("Right Arrow Pressed");
+    }
+  }
+  function handleKeyUp(event) {
+    if (event.which === KEY.UP) {
+      walker.speedY = 0;
+      console.log("Up Arrow Released");
+    }  
+    if (event.which === KEY.DOWN) {
+      walker.speedY = 0;
+      console.log("Down Arrow Released");
+    }  
+    if (event.which === KEY.LEFT) {
+      walker.speedX = 0;
+      console.log("Left Arrow Released");
+    }  
+    if (event.which === KEY.RIGHT) {
+      walker.speedX = 0;
+      console.log("Right Arrow Released");
+    }
+  }
+  function wallCollision(){
+    if(walker.positionX < 0){
+      walker.positionX = 0;
+      console.log("Border Hit");
+    }
+    if(walker.positionY < 0){
+      walker.positionY = 0;
+      console.log("Border Hit");
+    }
+    if(walker.positionX > borderWidth){
+      walker.positionX = borderWidth;
+      console.log("Border Hit");
+    }
+    if(walker.positionY > borderHeight){
+      walker.positionY = borderHeight;
+      console.log("Border Hit");
+    }
   }
 
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
-
+  function repositionGameItem() {
+    walker.positionX += walker.speedX;
+    walker.positionY += walker.speedY;
+  }
   
+  function redrawGameItem() {
+    $("#walker").css("left", walker.positionX);
+    $("#walker").css("right", walker.positionX);
+    $("#walker").css("top", walker.positionY);
+    $("#walker").css("bottom", walker.positionY);
+  }
+
   function endGame() {
     // stop the interval timer
     clearInterval(interval);
@@ -50,5 +124,4 @@ function runProgram(){
     // turn off event handlers
     $(document).off();
   }
-  
 }
